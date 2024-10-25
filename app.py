14f27030a7b92ecf4beb865c1c28b4cf2d70cc54
bibhu760 <158251787+bibhu760@users.noreply.github.com>
2024-10-25 17:34:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ import pickle
 model = pickle.load(open('model.pkl','rb'))
 
 st.header('Duplicate Question Pairs')
+<<<<<<< HEAD
 
 # Custom CSS to set background color
 
 
+=======
+>>>>>>> c4dea4e64612f1a066fe19fadc36494978db0cf3
 q1 = st.text_input('Enter question 1')
 q2 = st.text_input('Enter question 2')
 
